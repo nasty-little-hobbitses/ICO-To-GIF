70b18d80d09bab546b20bd7b4e1276f0a95f71ec
sarthakharne <75558601+sarthakharne@users.noreply.github.com>
2021-02-24 18:51:18 +0530

    Add files via upload

--- Proj.c
+++ Proj.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-#include"ourheader.h"
 
 /*--------------------------------------------------------------------------------------Defining Data Types---------------------------------------------------------------------------------------------------*/
 
 typedef unsigned char BYTE;
-typedef unsigned long DWORD;
 typedef unsigned short WORD;
+typedef unsigned int USIN;
 typedef long LONG;
 
 /*----------------------------------------------------------------------------------------End of Data Types---------------------------------------------------------------------------------------------------*/
@@ -22,45 +20,47 @@ typedef struct tagICONDIRENTRY
        BYTE bReserved;
        WORD wPlanes;
        WORD wBitCount;
-       unsigned int dwBytesInRes;
-       unsigned int dwImageOffset;
-}ICONDIRENTRY, *LPICONDIRENTRY;
+       USIN dwBytesInRes;
+       USIN dwImageOffset;
+}ICONDIRENTRY;
 
 typedef struct tagICONDIR
 {
-    WORD           idReserved;   // Reserved (must be 0)
-    WORD           idType;       // Resource Type (1 for icons)
-    WORD           idCount;      // How many images?
+	WORD           idReserved;      // Reserved (must be 0)
+	WORD           idType;          // Resource Type (1 for icons)
+	WORD           idCount;         // How many images?
 
-} ICONDIR, *LPICONDIR;
+} ICONDIR;
 
 typedef struct tagBITMAPINFOHEADER
 {
-unsigned int biSize;  //specifies the number of bytes required by the struct
-unsigned int biWidth;  //specifies width in pixels
-unsigned int biHeight;  //species height in pixels
-WORD biPlanes; //specifies the number of color planes, must be 1
-WORD biBitCount; //specifies the number of bit per pixel
- int biCompression;//spcifies the type of compression
- int biSizeImage;  //size of image in bytes
-double g1;
-double g2; //number of colors that are important
+	USIN 		biSize;         //specifies the number of bytes required by the struct
+	USIN 		biWidth;        //specifies width in pixels
+	USIN 		biHeight;       //species height in pixels
+	WORD 		biPlanes;       //specifies the number of color planes, must be 1
+	WORD 		biBitCount;     //specifies the number of bit per pixel
+	int 		biCompression;  //spcifies the type of compression
+	int 		biSizeImage;    //size of image in bytes
+	USIN            biXPelsPerMeter;
+        USIN            biYPelsPerMeter;
+        USIN            biClrUsed;
+        USIN            biClrImportant;
 }BITMAPINFOHEADER;
 
 typedef struct tagRGBQUAD {
-  BYTE rgbBlue;
-  BYTE rgbGreen;
-  BYTE rgbRed;
-  BYTE rgbReserved;
+	BYTE rgbBlue;
+	BYTE rgbGreen;
+	BYTE rgbRed;
+	BYTE rgbReserved;
 } RGBQUAD;
 
 typedef struct tagICONIMAGE {
 
-  BITMAPINFOHEADER   icHeader;      // DIB header
-   RGBQUAD         icColors[1];   // Color table
-   BYTE            icXOR[1];      // DIB bits for XOR mask
-   BYTE            icAND[1];      // DIB bits for AND mask
-} ICONIMAGE, *LPICONIMAGE;
+	BITMAPINFOHEADER   	icHeader;      // DIB header
+	RGBQUAD         	icColors[1];   // Color table
+	BYTE            	icXOR[1];      // DIB bits for XOR mask
+	BYTE            	icAND[1];      // DIB bits for AND mask
+} ICONIMAGE;
 
 
 
@@ -71,15 +71,15 @@ typedef struct tagICONIMAGE {
 // Structure contains "Header" & "Logical Screen Descriptor"
 typedef struct _GifHeader
 {
-  // Header
-  BYTE Signature[3];     /* Header Signature (always "GIF") */
-  BYTE Version[3];       /* GIF format version("87a" or "89a") */
-  // Logical Screen Descriptor
-  WORD ScreenWidth;      /* Width of Display Screen in Pixels */
-  WORD ScreenHeight;     /* Height of Display Screen in Pixels */
-  BYTE Packed;           /* Screen and Color Map Information */
-  BYTE BackgroundColor;  /* Background Color Index */
-  BYTE AspectRatio;      /* Pixel Aspect Ratio */
+	// Header
+	BYTE Signature[3];     /* Header Signature (always "GIF") */
+	BYTE Version[3];       /* GIF format version("87a" or "89a") */
+	// Logical Screen Descriptor
+	WORD ScreenWidth;      /* Width of Display Screen in Pixels */
+	WORD ScreenHeight;     /* Height of Display Screen in Pixels */
+	BYTE Packed;           /* Screen and Color Map Information */
+	BYTE BackgroundColor;  /* Background Color Index */
+	BYTE AspectRatio;      /* Pixel Aspect Ratio */
 } GIFHEAD;
 
 // Backbone Structure for colour entries of both Global and Local Colour Table
@@ -171,10 +171,6 @@ int main(int argc, char *argv[])
 	header->Packed = 247;
 	fwrite(&(header->Packed),sizeof(BYTE),1,fw);
 
-	// to calculate size of global color table
-	int SizeOfGCT = three_bit(header->Packed,0);
-	int NoOfGCTEnt = (1L << (SizeOfGCT + 1));
-
 	// Background Color = 0
 	header->BackgroundColor = 0;
 	fwrite(&(header->BackgroundColor),sizeof(BYTE),1,fw);
@@ -183,36 +179,34 @@ int main(int argc, char *argv[])
 	header->AspectRatio = 0;
 	fwrite(&(header->AspectRatio),sizeof(BYTE),1,fw);
 
-	// Global Colour Table reading and writing till index no 127
+	// Global Colour Table reading and writing
 	RGBQUAD arr[256];
 	for(i = 0;i < 256;i++)
 	{
 		fread(&arr[i], sizeof(RGBQUAD),1,fr);
 
-
-		        fwrite(&(arr[i].rgbRed),sizeof(BYTE),1,fw);
-		        fwrite(&(arr[i].rgbGreen),sizeof(BYTE),1,fw);
-		        fwrite(&(arr[i].rgbBlue),sizeof(BYTE),1,fw);
-
+		fwrite(&(arr[i].rgbRed),sizeof(BYTE),1,fw);
+		fwrite(&(arr[i].rgbGreen),sizeof(BYTE),1,fw);
+		fwrite(&(arr[i].rgbBlue),sizeof(BYTE),1,fw);
 	}
 
 	// To accomodate for padding, height and width need to be inc accordingly (multiple of 4)
 
-	if(y->bWidth % 4 != 0){
+	if(y->bWidth % 4 != 0)
         	y->bWidth += 4 - y->bWidth%4;
-        }
+	
+	// 2-D Array for Image data
 	BYTE bits[y->bHeight][y->bWidth];
 
 	// reading image data from ICO from last row
 	for(int i = y->bHeight - 1;i >= 0;i--)
-        {
                 for(int j = 0;j < y->bWidth;j++)
                 {
 	                fread(&bits[i][j],1,1,fr);
-                        if (bits[i][j] > 127)
+                        
+			if (bits[i][j] > 127)
                                 bits[i][j] = 32;
                 }
-	}
 
 	// Local Image Descriptor
 	GIFIMGDESC *img;
@@ -261,26 +255,10 @@ int main(int argc, char *argv[])
 	free(y);
 	free(z);
 	free(header);
+	free(img);
 	fclose(fr);
 	fclose(fw);
 
 
 	return 0;
-}
-
-// Returns the nth bit of number in binary form
-int nth_bit(int a,int n)
-{
-	return (a>>n & 1);
-}
-
-// Returns decimal form of 3 bits starting with lsb as nth bit Eg: 111 is returned as 7
-int three_bit(int a,int n)
-{
-	int num = 0,i;
-	for(i=n;i<n+3;i++)
-	{
-		num += pow(2,i-n) * (a>>i & 1);
-	}
-	return num;
 }
